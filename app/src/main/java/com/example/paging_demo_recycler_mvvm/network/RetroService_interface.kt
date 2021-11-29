2d5a7e61252fa8a935f96b8daeb11e51121735c3
spyboy11 <52691677+spyboy11@users.noreply.github.com>
2021-11-29 15:03:50 +0530

    upload it

--- app/src/main/java/com/example/paging_demo_recycler_mvvm/network/RetroService_interface.kt
+++ app/src/main/java/com/example/paging_demo_recycler_mvvm/network/RetroService_interface.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Query
 
 interface RetroService_interface {
 
-    @GET("users")    //users?page=2
+    @GET("top_rated?api_key=ae7a6878f877a48fb63853582fa3d034&language=en-US&page=1")    //users?page=2, top_rated?api_key=ae7a6878f877a48fb63853582fa3d034&language=en-US&page=1
     fun getDataFromApi(@Query("page") page: Int): Call<apiDataClass>
 }
